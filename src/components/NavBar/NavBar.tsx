49f58fcb871bc4140b2ee3bf82c4bb5330167e9f
livehass <silva.felipe12@hotmail.com>
2024-03-23 13:57:12 -0300

    added project components

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,10 +4,10 @@ import { Link, useNavigate } from 'react-router-dom'
 function Navbar() {
 
     return (
-        <nav className="bg-white dark:bg-transparent fixed w-full z-20 top-0 start-0 shadow-lg shadow-slate-950 ">
+        <nav className="bg-white dark:bg-transparent fixed w-full z-20 top-0 start-0 shadow-lg">
    <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a
-      href="https://github.com/livehass"
+      href="/"
       className="flex items-center space-x-3 rtl:space-x-reverse"
     >
       <img
@@ -15,7 +15,7 @@ function Navbar() {
         className="h-8"
         alt="logo react"
       />
-      <span className="self-center text-3xl font-semibold whitespace-nowrap dark:text-white hover:text-blue-700">
+      <span className="shadow-lg self-center text-3xl font-semibold whitespace-nowrap dark:text-white hover:text-blue-700">
         JF
       </span>
     </a>
@@ -23,31 +23,49 @@ function Navbar() {
       className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
       id="navbar-sticky"
     >
-      <ul className="flex flex-col text-x3 p-4 md:p-1 mt-4 font-medium border border-gray-100 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 ">
+      <ul className="flex shadow-lg flex-col text-x4 p-4 md:p-1 mt-4 font-medium border border-gray-100 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 ">
         <li>
           <a
-            href="#"
-            className="block text-x3 py-2 px-4 text-white  hover:text-blue-700 "
+            className="block text-x4 py-2 px-4 text-white  hover:text-blue-700 "
             aria-current="page"
+            onClick={() => {
+              const element = document.getElementById('home')
+              element.scrollIntoView({
+                behavior: 'smooth'
+              })
+            }}
           >
-            <Link to='/Home' className='hover:underline'>HOME</Link>
+            <button className='hover:underline'>HOME</button>
           </a>
         </li>
         <li>
-           
           <a
-            href="/"
+            onClick={() => {
+              const element = document.getElementById('about');
+              element.scrollIntoView({
+              behavior: 'smooth'
+
+              })
+            }}
             className="block py-2 px-4 text-white hover:text-blue-700"
           >
-            <Link to='/Temas' className='hover:underline'>ABOUT ME</Link>
+            <Link to='' className='hover:underline'>ABOUT ME</Link>
           </a>
         </li>
         <li>
           <a
-            href="#"
-            className="block py-2 px-4  md:hover:bg-transparent  md:p-2 text-white hover:text-blue-700"
+            
+            className="block py-2 px-4 text-x4  md:hover:bg-transparent  md:p-2 text-white hover:text-blue-700"
           >
-            <Link to='/CadastroTema' className='hover:underline'>PROJECTS</Link>
+            <button 
+             onClick={() => {
+              const element = document.getElementById('projects');
+              element.scrollIntoView({
+              behavior: 'smooth'
+
+              })
+            }}
+            className='hover:underline'>PROJECTS</button>
           </a>
         </li>  
       </ul>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,31 +1,41 @@
-import React from 'react';
-import homeLogo from '../../assets/home.png'
 import './Home.css';
+import About from '../../components/about/About';
+import Projects from '../../components/projects/Projects';
+
 
 import { Link } from 'react-router-dom';
 
 function Home() {
     return (
     <>
-    <header className='grid gap-4 grid-cols-2 font-bold content-center w-auto m-64'>
+    <header id="home" className='grid gap-4 grid-cols-2 font-bold content-center w-auto m-64'>
       <div className='text-left w-auto text-balance mt-5'>
           <div className='mt-20 ml-40'>
             <h1 className=' text-slate-400 text-7x1'>Hello Friend I'm</h1>
             <h2 className='text-white text-9xl mb-10'>Jorge Felipe,</h2>
             <p className='text-slate-400 text-balance w-79 text-7x1 '>A passionate fullstack developer focused in create front end apps in react and backend with java spring.</p>
-           
-            <button  className='border rounded-lg px-6 bg-blue-700  hover:text-blue-900 py-3 mr-2  mt-4'>
-                <Link to='/postagens' className='hover:underline text-white'>See my work</Link>
+            
+            <button className='border rounded-lg px-6 bg-blue-700  hover:text-blue-900 py-3 mr-2  mt-4'
+              onClick={() => {
+                const element = document.getElementById('projects');
+                element.scrollIntoView({
+                  behavior: 'smooth'
+                })
+              }}
+            >
+                <Link to='' className='hover:underline text-white'>See my work</Link>
               </button>
               <button  className='border rounded-lg px-6 hover:text-blue-900 border-blue-700 py-3 ml-6 mt-2'>
-                <Link to='/postagens' className='hover:underline text-white'>View Resume</Link>
+                <Link to='projects' className='hover:underline text-white'>View Resume</Link>
               </button>
           </div>
-       </div>
-       <div className='w-auto mr-40 mt-11'>
-            <img src="https://raw.githubusercontent.com/livehass/blog-pessoal-react/main/src/assets/proglogo.png" alt="" />
-       </div>
+        </div>
+        <div className='w-auto mr-40 mt-11'>
+            <img src="/src/assets/home.svg" alt="Home Logo" />
+        </div>
     </header>
+    <About/>
+    <Projects/>
     </>
     );
 }

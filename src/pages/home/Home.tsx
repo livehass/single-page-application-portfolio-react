42c3ee08ce2164c4f380e5d73d9038341d546b1e
livehass <silva.felipe12@hotmail.com>
2024-04-29 16:02:53 -0300

    Added new route to resume in home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,48 +1,58 @@
-import './Home.css';
-import About from '../../components/about/About';
-import Projects from '../../components/projects/Projects';
-import { useRef, useEffect, useState } from 'react';
-import { useInView } from 'react-intersection-observer';
+import "./Home.css";
+import About from "../../components/about/About";
+import Projects from "../../components/projects/Projects";
+import { useRef, useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
 
-
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Home() {
-    const { ref: myRef, inView: myElementIsVisible } = useInView();
-    const { ref: hRef, inView: titleIsVisible } = useInView();
+  const { ref: myRef, inView: myElementIsVisible } = useInView();
+  const { ref: hRef, inView: titleIsVisible } = useInView();
 
-    return (
+  return (
     <>
-    <header id="home" className='grid gap-4 grid-cols-2 font-bold content-center w-auto m-64'>
-      <div className='text-left w-auto text-balance mt-5'>
-          <div className='mt-20 ml-40'>
-            <h1 ref={hRef} className=' text-slate-400 text-7x1'>Hello Friend I'm</h1>
-            <h2 className='text-white text-9xl mb-10'>Jorge Felipe,</h2>
-            <p className='text-slate-400 text-balance w-79 text-7x1 '>A passionate fullstack developer focused in create front end apps in react and backend with java spring.</p>
-            
-            <button className='border rounded-lg px-6 bg-blue-700  hover:text-blue-900 py-3 mr-2  mt-4'
+      <header
+        id="home"
+        className="grid gap-4 grid-cols-2 font-bold content-center w-auto m-64"
+      >
+        <div className="text-left w-auto text-balance mt-5">
+          <div className="mt-20 ml-40">
+            <h1 ref={hRef} className=" text-slate-400 text-7x1">
+              Hello Friend I'm
+            </h1>
+            <h2 className="text-white text-9xl mb-10">Jorge Felipe,</h2>
+            <p className="text-slate-400 text-balance w-79 text-7x1 ">
+              A passionate fullstack developer focused in create front end apps
+              in react and backend with java spring.
+            </p>
+
+            <button
+              className="border rounded-lg px-6 bg-blue-700  hover:text-blue-900 py-3 mr-2  mt-4"
               onClick={() => {
-                const element = document.getElementById('projects');
+                const element = document.getElementById("projects");
                 element.scrollIntoView({
-                  behavior: 'smooth'
-                })
+                  behavior: "smooth",
+                });
               }}
             >
-                <Link to='' className='hover:underline text-white'>See my work</Link>
-              </button>
-              <button  className='border rounded-lg px-6 hover:text-blue-900 border-blue-700 py-3 ml-6 mt-2'>
-                <Link to='projects' className='hover:underline text-white'>View Resume</Link>
-              </button>
+              <Link to="" className="hover:underline text-white">
+                See my work
+              </Link>
+            </button>
+            <button className="border rounded-lg px-6 hover:text-blue-900 border-blue-700 py-3 ml-6 mt-2">
+              <Link to="/Resume" className="hover:underline text-white">
+                View Resume
+              </Link>
+            </button>
           </div>
         </div>
-        <div className='w-auto mr-40 mt-11'>
-            <img src="/src/assets/home.svg" alt="Home Logo" />
+        <div className="w-auto mr-40 mt-11">
+          <img src="/src/assets/home.svg" alt="Home Logo" />
         </div>
-    </header>
-    <About/>
-    <Projects/>
+      </header>
     </>
-    );
+  );
 }
 
 export default Home;
